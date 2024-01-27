871991fc3364314222f82a9f53cfbdb885e4a8f8
RonaldoML <32518876+RonaldoML@users.noreply.github.com>
2024-01-27 14:48:27 -0500

    Revert "Api/ft/add swagger"

--- challenge-api/README.md
+++ challenge-api/README.md
@@ -8,5 +8,3 @@ In the project directory, you can run:
 
 Runs the app in the development mode.\
 Open [http://localhost:4000](http://localhost:4000) to view it in your browser.
-
-Open [http://localhost:4000/api-doc](http://localhost:4000/api-doc) to view documentation in your browser.
--- challenge-api/src/models/server.js
+++ challenge-api/src/models/server.js
@@ -1,35 +1,4 @@
 const express = require('express');
-const path = require('path');
-const swaggerUI = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
-
-
-const swaggerSpec = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Challenge API',
-      version: '1.0.1',
-    },
-    servers: [
-      {
-        url: 'http://localhost:{port}',
-        description: 'Development server',
-        variables: {
-          port: {
-            enum: [
-              4000
-            ],
-            default: 4000
-          }
-        }
-      }
-    ]
-  },
-  apis: [
-    `${path.join(__dirname, "../routes/files.js")}`
-  ],
-}
 
 class Server {
 
@@ -38,19 +7,7 @@ class Server {
     this.port = 4000;
 
     this.routes();
-    this.middlewares();
   }
-
-  middlewares() {
-    this.app.use(
-      '/api-doc',
-      swaggerUI.serve,
-      swaggerUI.setup(
-        swaggerJsDoc(
-          swaggerSpec
-        )));
-
-  };
 
   routes() {
     this.app.use('/files', require('../routes/files'));
@@ -59,15 +16,15 @@ class Server {
       res.send('Hello World!')
     });
 
-  };
+  }
 
   listen() {
     this.app.listen(
       this.port,
       () => console.log('App is listening on port: ', this.port)
     );
-  };
-};
+  }
+}
 
 module.exports = Server;
 
